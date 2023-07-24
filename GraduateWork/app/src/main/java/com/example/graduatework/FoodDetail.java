07b79656c87884253d77a3bae98afe87dbf9ad93
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-07-25 02:01:28 +0300

    food details done

--- GraduateWork/app/src/main/java/com/example/graduatework/FoodDetail.java
+++ GraduateWork/app/src/main/java/com/example/graduatework/FoodDetail.java
@@ -1,14 +1,23 @@
 package com.example.graduatework;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.graduatework.database.Food;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class FoodDetail extends AppCompatActivity {
     TextView foodName_det, foodPrice_det, foodAmount_det, foodDesc_det;
@@ -16,9 +25,13 @@ public class FoodDetail extends AppCompatActivity {
     CollapsingToolbarLayout collapsing;
     FloatingActionButton btnCart;
 
+    Food food;
+
     //elegant num btn
     TextView count_item;
     ImageView btnRemoveItem, btnAddItem;
+    int totalItemCount=1;
+    int totalPrice=0;
 
     String foodId = "";
 
@@ -29,5 +42,79 @@ public class FoodDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+
+        database = FirebaseDatabase.getInstance().getReference("Food");
+
+
+        //init
+        foodName_det = (TextView) findViewById(R.id.foodName_det);
+        foodPrice_det = (TextView) findViewById(R.id.foodPrice_det);
+        foodAmount_det = (TextView) findViewById(R.id.foodAmount_det);
+        foodDesc_det = (TextView) findViewById(R.id.foodDescription_det);
+        foodImage_det = (ImageView) findViewById(R.id.img_food_det);
+        collapsing =(CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        btnCart=(FloatingActionButton) findViewById(R.id.btnCart_det);
+        count_item=(TextView) findViewById(R.id.count_of_item);
+        btnRemoveItem= (ImageView) findViewById(R.id.btn_remove_item);
+        btnAddItem= (ImageView) findViewById(R.id.btn_add_item);
+
+        //get init
+        if (getIntent()!=null)
+            foodId = getIntent().getStringExtra("FoodId");
+        if (!foodId.isEmpty()){
+            //get details
+            database.child(foodId).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                    Food food = snapshot.getValue(Food.class);
+                    food = snapshot.getValue(Food.class);
+
+                    //set
+                    Picasso.with(getBaseContext()).load(food.getImage()).into(foodImage_det);
+
+//                    collapsing.setTitle(food.getName());
+                    foodName_det.setText(food.getName());
+                    foodAmount_det.setText(food.getAmount());
+                    foodDesc_det.setText(food.getDescription());
+                    foodPrice_det.setText(String.valueOf(Integer.parseInt(food.getPrice()) * totalItemCount));
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalItemCount < 10){
+                    totalItemCount++;
+                    count_item.setText(String.valueOf(totalItemCount));
+                    foodPrice_det.setText(String.valueOf(Integer.parseInt(food.getPrice()) * totalItemCount));
+                }
+            }
+        });
+
+        btnRemoveItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalItemCount > 1){
+                    totalItemCount--;
+                    count_item.setText(String.valueOf(totalItemCount));
+                    foodPrice_det.setText(String.valueOf(Integer.parseInt(food.getPrice()) * totalItemCount));
+
+                }
+            }
+        });
+
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
