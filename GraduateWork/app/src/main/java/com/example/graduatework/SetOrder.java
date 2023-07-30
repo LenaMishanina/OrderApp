6d35c5f8494eb5eaed4919ea2102ba98c70f2f46
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-07-30 16:39:34 +0300

    design activity set order

---
+++ GraduateWork/app/src/main/java/com/example/graduatework/SetOrder.java
@@ -0,0 +1,14 @@
+package com.example.graduatework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_order);
+    }
+}
--- GraduateWork/app/src/main/java/com/example/graduatework/database/Request.java
+++ GraduateWork/app/src/main/java/com/example/graduatework/database/Request.java
@@ -3,19 +3,41 @@ package com.example.graduatework.database;
 import java.util.List;
 
 public class Request {
-    private String phone, name, address, total, status;
+    private String phone, name, address, total, status, comment, payment_option;
     private List<Order> orderFoods;
 
     public Request() {
     }
 
-    public Request(String phone, String name, String address, String total, List<Order> orderFoods) {
+    public Request(String phone, String name, String address, String total, String comment, String payment_option, List<Order> orderFoods) {
         this.phone = phone;
         this.name = name;
         this.address = address;
         this.total = total;
+        this.status = "0"; // 0 - размещен; 1 - доставляется; 2 - доставлен
+        this.comment = comment;
+        this.payment_option = payment_option;
         this.orderFoods = orderFoods;
-        this.status="0"; // 0 - размещен; 1 - доставляется; 2 - доставлен
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setPayment_option(String payment_option) {
+        this.payment_option = payment_option;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getPayment_option() {
+        return payment_option;
     }
 
     public String getStatus() {
