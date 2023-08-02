e4ca704241c36c7faf5ca3b89037f8304ec13596
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-08-03 00:00:09 +0300

    Ready sign in & up

--- GraduateWork/app/src/main/java/com/example/graduatework/SignIn.java
+++ GraduateWork/app/src/main/java/com/example/graduatework/SignIn.java
@@ -3,10 +3,12 @@ package com.example.graduatework;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.icu.util.LocaleData;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -17,6 +19,7 @@ import com.example.graduatework.Common.Common;
 import com.example.graduatework.database.User;
 import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,9 +30,14 @@ import io.paperdb.Paper;
 
 public class SignIn extends AppCompatActivity {
     TextInputEditText edtPhone, edtPassword;
+    TextView txtToSignUp;
     Button btnSignIn;
     CheckBox checkBox;
 
+    TextInputLayout txtInpLayoutPhone, txtInpLayoutPwd;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +46,10 @@ public class SignIn extends AppCompatActivity {
         edtPassword = (TextInputEditText) findViewById(R.id.edtPassword);
         btnSignIn = (Button) findViewById(R.id.btnSignIn);
         checkBox = (CheckBox) findViewById(R.id.cb_rem_me);
+        txtToSignUp = (TextView) findViewById(R.id.txtToRegistration);
+
+        txtInpLayoutPhone = (TextInputLayout) findViewById(R.id.txtInpLayoutPhone);
+        txtInpLayoutPwd = (TextInputLayout) findViewById(R.id.txtInpLayoutPwd);
 
         //Init paper
 
@@ -46,6 +58,14 @@ public class SignIn extends AppCompatActivity {
         //Init Firebase
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
+
+        txtToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,41 +77,53 @@ public class SignIn extends AppCompatActivity {
                     Paper.book().write(Common.PASSWD_KEY, edtPassword.getText().toString());
                 }
 
-                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
-                mDialog.setMessage("Please waiting...");
-                mDialog.show();
+                if (!TextUtils.isEmpty(edtPhone.getText())) {
 
-                table_user.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
-                            mDialog.dismiss();
-                            //Get user info
-                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);//get user
-                            user.setPhone(edtPhone.getText().toString());
-                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                    if (!TextUtils.isEmpty(edtPassword.getText())) {
 
-                                Intent home = new Intent(SignIn.this, Home.class);
-                                Common.currentUser=user;
-                                startActivity(home);
-                                finish();
+                        final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                        mDialog.setMessage("Пожалуйста, подождите...");
+                        mDialog.show();
+
+                        table_user.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                                    mDialog.dismiss();
+                                    //Get user info
+                                    User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);//get user
+                                    user.setPhone(edtPhone.getText().toString());
+                                    if (user.getPassword().equals(edtPassword.getText().toString())) {
+
+                                        Intent home = new Intent(SignIn.this, Home.class);
+                                        Common.currentUser = user;
+                                        startActivity(home);
+                                        finish();
 
 
-                            } else {
-                                Toast.makeText(SignIn.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(SignIn.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    mDialog.dismiss();
+                                    Toast.makeText(SignIn.this, "User not exist", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                        else {
-                            mDialog.dismiss();
-                            Toast.makeText(SignIn.this, "User not exist", Toast.LENGTH_SHORT).show();
-                        }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                    } else{
+                        txtInpLayoutPwd.setHelperText("Обязательное поле*");
                     }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+                }else {
+                    txtInpLayoutPhone.setHelperText("Обязательное поле*");
+                }
 
-                    }
-                });
             }
         });
     }
--- GraduateWork/app/src/main/java/com/example/graduatework/SignUp.java
+++ GraduateWork/app/src/main/java/com/example/graduatework/SignUp.java
@@ -3,8 +3,11 @@ package com.example.graduatework;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 
 import com.example.graduatework.database.User;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +24,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class SignUp extends AppCompatActivity {
     TextInputEditText edtPhone, edtName, edtPassword;
+    TextView txtAlreadyLogin;
     Button btnSingUp;
+    TextInputLayout txtInpLayoutPhoneReg,txtInpLayoutNameReg,txtInpLayoutPwdReg;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,41 +39,71 @@ public class SignUp extends AppCompatActivity {
         edtName = (TextInputEditText) findViewById(R.id.edtName);
         edtPassword = (TextInputEditText) findViewById(R.id.edtPassword);
         btnSingUp = (Button) findViewById(R.id.btnSignUp);
+        txtAlreadyLogin = (TextView) findViewById(R.id.txtAlreadyLogin);
+        txtInpLayoutPhoneReg = (TextInputLayout) findViewById(R.id.txtInpLayoutPhoneReg);
+        txtInpLayoutNameReg = (TextInputLayout) findViewById(R.id.txtInpLayoutNameReg);
+        txtInpLayoutPwdReg = (TextInputLayout) findViewById(R.id.txtInpLayoutPwdReg);
 
         //init firebase
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
 
+        txtAlreadyLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
         btnSingUp.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
-                mDialog.setMessage("Please waiting...");
-                mDialog.show();
 
-                table_user.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        //check if already exist
-                        if (snapshot.child(edtPhone.getText().toString()).exists()){
-                            mDialog.dismiss();
-                            Toast.makeText(SignUp.this, "phone number already register", Toast.LENGTH_SHORT).show();
+                if (!TextUtils.isEmpty(edtPhone.getText())) {
+
+                    if (!TextUtils.isEmpty(edtName.getText())) {
+
+                        if (!TextUtils.isEmpty(edtPassword.getText())) {
+
+                            final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                            mDialog.setMessage("Please waiting...");
+                            mDialog.show();
+
+                            table_user.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    //check if already exist
+                                    if (snapshot.child(edtPhone.getText().toString()).exists()) {
+                                        mDialog.dismiss();
+                                        Toast.makeText(SignUp.this, "phone number already register", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        mDialog.dismiss();
+                                        User user = new User(edtName.getText().toString(), edtPassword.getText().toString());
+                                        table_user.child(edtPhone.getText().toString()).setValue(user);//create new user
+                                        Toast.makeText(SignUp.this, "Sign Up succeed", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
                         } else {
-                            mDialog.dismiss();
-                            User user = new User(edtName.getText().toString(), edtPassword.getText().toString());
-                            table_user.child(edtPhone.getText().toString()).setValue(user);//create new user
-                            Toast.makeText(SignUp.this, "Sign Up succeed", Toast.LENGTH_SHORT).show();
-                            finish();
+                            txtInpLayoutPwdReg.setHelperText("Обязательное поле*");
                         }
 
+                    } else {
+                        txtInpLayoutNameReg.setHelperText("Обязательное поле*");
                     }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                }else {
+                    txtInpLayoutPhoneReg.setHelperText("Обязательное поле*");
+                }
             }
         });
     }
